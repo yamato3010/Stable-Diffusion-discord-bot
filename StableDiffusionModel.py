5d3811697aa731092a4eece08ddce5de7e4585d8
yamato3010 <yama3to10yama3@gmail.com>
2022-08-30 20:47:08 +0900

    日本語の文章でも生成されるように

--- StableDiffusionModel.py
+++ StableDiffusionModel.py
@@ -5,19 +5,22 @@ import torch
 from diffusers import StableDiffusionPipeline
 from torch import autocast
 from dotenv import load_dotenv
+from transformers import pipeline
 load_dotenv()
  
 args = sys.argv
 MODEL_ID = "CompVis/stable-diffusion-v1-4"
 DEVICE = "cuda"
 YOUR_TOKEN = os.environ.get("API_TOKEN")
+translator = pipeline('translation', model='staka/fugumt-ja-en')
 
 def generate(prompt):
   pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, revision="fp16", torch_dtype=torch.float16, use_auth_token=YOUR_TOKEN)
   pipe.to(DEVICE)
-
+  translated = translator(prompt)[0]['translation_text']
+  print(translated)
   with autocast(DEVICE):
-    image = pipe(prompt, guidance_scale=7.5)["sample"][0]
+    image = pipe(translated, guidance_scale=7.5)["sample"][0]
     result = image.save("test.png")
     return result
 
